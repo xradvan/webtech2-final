2a92243a1a068fc7437b94821ea258b431f84efb
xradvan <xradvan@stuba.sk>
2018-05-14 21:43:18 +0200

    sql export

---
+++ MySQL/webtech2_final.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2018 at 07:37 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webtech2_final`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pouzivatelia`
+--
+
+CREATE TABLE `pouzivatelia` (
+  `id` int(11) NOT NULL,
+  `meno` varchar(20) COLLATE utf8_slovak_ci NOT NULL,
+  `priezvisko` varchar(20) COLLATE utf8_slovak_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
+  `stredna_skola` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
+  `stredna_skola_adresa` varchar(60) COLLATE utf8_slovak_ci NOT NULL,
+  `ulica` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
+  `psc` varchar(10) COLLATE utf8_slovak_ci NOT NULL,
+  `obec` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pouzivatelia`
+--
+ALTER TABLE `pouzivatelia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pouzivatelia`
+--
+ALTER TABLE `pouzivatelia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -3,5 +3,3 @@
  * Index file
  *
  */
-
-echo "Hello World";
