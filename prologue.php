97938634e19e258b51b87295ceee086dc5b0acac
MarcelBoldis <marcelboldis@gmail.com>
2018-05-14 21:14:53 +0200

    pridany php script

--- prologue.php
+++ prologue.php
@@ -7,17 +7,39 @@
     <title>Úvodná stránka</title>
 </head>
 <body>
-<input type="button" value="Adresa Userov">
-<input type="button" value="Adresa Skol">
+<form>
+    <input type="submit" value="Adresa Userov" name="adresa_uzivatel">
+    <input type="submit" value="Adresa Skol" name="adresa_skoly">
+</form>
 
 <div id="googleMap" style="width:50%;height:350px;"></div>
 <script src="scripts/map.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCzAGVYxB7GHyFD0rDQuIiKZMWiYKWBqsw&callback=myMap"></script>
-</body>
-</html>
-
-
 
 <?php
+require ('config.php');
+/*$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("UTF8");
+if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}*/
+
+if (isset($_GET['adresa_uzivatel'])) {
+    echo "Je vyplnena adresa uzivatela";
+
+    /*$query="SELECT * from User";
+    $result = mysqli_query($conn,$query);
+
+    while ($data = mysqli_fetch_array($result))
+    {
+        $data['name'];
+    }*/
+
+}
+elseif (isset($_GET['adresa_skoly'])) {
+    echo "Je vyplnena adresa skoly";
+}
+
 
 ?>
+
+</body>
+</html>
