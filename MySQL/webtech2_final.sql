953181165776a32549998d8f2eb5cc86d54e2bbb
xradvan <xradvan@stuba.sk>
2018-05-14 22:01:09 +0200

    sql export

--- MySQL/webtech2_final.sql
+++ MySQL/webtech2_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2018 at 07:37 PM
+-- Generation Time: May 14, 2018 at 07:59 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -36,8 +36,16 @@ CREATE TABLE `pouzivatelia` (
   `ulica` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
   `psc` varchar(10) COLLATE utf8_slovak_ci NOT NULL,
   `obec` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
-  `admin` tinyint(1) NOT NULL
+  `rola` varchar(10) COLLATE utf8_slovak_ci NOT NULL DEFAULT 'user'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci;
+
+--
+-- Dumping data for table `pouzivatelia`
+--
+
+INSERT INTO `pouzivatelia` (`id`, `meno`, `priezvisko`, `email`, `stredna_skola`, `stredna_skola_adresa`, `ulica`, `psc`, `obec`, `rola`) VALUES
+(1, 'Petor', 'Retvar', 'petorko@gmail.com', 'GCM', 'Farska 10', 'Lipova', '95193', 'TPK', 'user'),
+(2, 'Marcel', 'Boldis', 'marcelko@pekny.sk', 'Nedokoncena', 'Gymnazium Vrable', 'Skolska', '84321', 'Vozokany', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -57,7 +65,7 @@ ALTER TABLE `pouzivatelia`
 -- AUTO_INCREMENT for table `pouzivatelia`
 --
 ALTER TABLE `pouzivatelia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
