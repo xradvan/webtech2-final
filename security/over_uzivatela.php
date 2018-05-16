872deb48a89f6e918b4ae09550e4cb917467344c
RudeBoyxX <matusbubeliny@gmail.com>
2018-05-16 13:40:19 +0200

    ak pouzivatel nie je prihlaseny, presmeruje ho na uvodnu stranku

--- security/over_uzivatela.php
+++ security/over_uzivatela.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Pridanim tohto suboru sa skontroluje, ci je uzivatel prihlaseny.
- * Ak nie, presmeruje ho na prihlasenie
+ * Ak nie, presmeruje ho na uvodnu stranku
  */
 
 session_start();
 
 if (!isset($_SESSION["rola"])) {
-    header("Location: security/prihlasenie.php");
+    header("Location: prologue.php");
     die();
 }
