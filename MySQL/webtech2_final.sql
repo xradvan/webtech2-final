5fed1f8ba4c09cba64564a42820e768ddce6a7db
xradvan <xradvan@stuba.sk>
2018-05-16 10:40:51 +0200

    db export

--- MySQL/webtech2_final.sql
+++ MySQL/webtech2_final.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: May 14, 2018 at 07:59 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: localhost
+-- Generation Time: May 16, 2018 at 10:56 AM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,6 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `aktuality`
+--
+
+CREATE TABLE `aktuality` (
+  `id` int(11) NOT NULL,
+  `titulok` varchar(100) COLLATE utf8_slovak_ci NOT NULL,
+  `obsah` varchar(500) COLLATE utf8_slovak_ci NOT NULL,
+  `datum` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci;
+
+--
+-- Dumping data for table `aktuality`
+--
+
+INSERT INTO `aktuality` (`id`, `titulok`, `obsah`, `datum`) VALUES
+(15, 'jdnfcjdncx', 'cxjvnjcv', '2018-05-15'),
+(18, 'mxcmx cmxc', 'x cmxc mxc xm', '2018-05-15'),
+(19, 'zxcnxjncc', 'knckxcnkxnc', '2018-05-15'),
+(20, 'jxnvjcvnc', 'xncxkvnxk', '2018-05-15');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pouzivatelia`
 --
 
@@ -31,28 +54,33 @@ CREATE TABLE `pouzivatelia` (
   `meno` varchar(20) COLLATE utf8_slovak_ci NOT NULL,
   `priezvisko` varchar(20) COLLATE utf8_slovak_ci NOT NULL,
   `email` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
+  `heslo` varchar(60) COLLATE utf8_slovak_ci NOT NULL,
   `stredna_skola` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
   `stredna_skola_adresa` varchar(60) COLLATE utf8_slovak_ci NOT NULL,
   `ulica` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
   `psc` varchar(10) COLLATE utf8_slovak_ci NOT NULL,
   `obec` varchar(40) COLLATE utf8_slovak_ci NOT NULL,
-  `rola` varchar(10) COLLATE utf8_slovak_ci NOT NULL DEFAULT 'user'
+  `rola` varchar(10) COLLATE utf8_slovak_ci NOT NULL DEFAULT 'user',
+  `odoberatel` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_slovak_ci;
 
 --
 -- Dumping data for table `pouzivatelia`
 --
 
-INSERT INTO `pouzivatelia` (`id`, `meno`, `priezvisko`, `email`, `stredna_skola`, `stredna_skola_adresa`, `ulica`, `psc`, `obec`, `rola`) VALUES
-(1, 'Petor', 'Retvar', 'petorko@gmail.com', 'GCM', 'Farska 10', 'Lipova', '95193', 'TPK', 'user'),
-(2, 'Marcel', 'Boldis', 'marcelko@pekny.sk', 'Nedokoncena', 'Gymnazium Vrable', 'Skolska', '84321', 'Vozokany', 'admin');
+INSERT INTO `pouzivatelia` (`id`, `meno`, `priezvisko`, `email`, `heslo`, `stredna_skola`, `stredna_skola_adresa`, `ulica`, `psc`, `obec`, `rola`, `odoberatel`) VALUES
+(5, 'Marek', 'Blaha', 'blaha.marcus@gmail.com', '$2y$10$NyDMP1WJtI.xAasNonHgGO2BY2HjefsbnG11.AUpSc2XK1EF9BPea', 'GYM', 'Štefana Moyzesa 21, 034 01, Ružomberok', 'Nizna Revuca, 36', '03474', 'Liptovske Revuce', 'user', 0),
+(4, 'Peter', 'Radvan', 'peterradvan@gmail.com', '$2y$10$Wlyw5xYwJsQfm/rd7N9msecqHdMSeLAwGnmoCCgvxfcaEfrLXtdDG', 'GCM', 'Farska 10, Nitra', 'Lipova 19', '95193', 'Topolcianky', 'user', 0);
 
 --
 -- Indexes for dumped tables
 --
 
-
-
+--
+-- Indexes for table `aktuality`
+--
+ALTER TABLE `aktuality`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `pouzivatelia`
@@ -65,10 +93,15 @@ ALTER TABLE `pouzivatelia`
 --
 
 --
+-- AUTO_INCREMENT for table `aktuality`
+--
+ALTER TABLE `aktuality`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
 -- AUTO_INCREMENT for table `pouzivatelia`
 --
 ALTER TABLE `pouzivatelia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
