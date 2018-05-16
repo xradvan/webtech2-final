7e39fb871a96e2e4f8bea5dc4f192f0a4306c951
marek-blaha <xblaham1@stuba.sk>
2018-05-16 15:29:50 +0200

    cesty plus trasa

--- cesty.php
+++ cesty.php
@@ -1,11 +1,5 @@
 <?php
-    include("security/over_uzivatela.php");
-    if(isset($_GET['odhlasenie'])){
-    session_destroy();
-    unset($_SESSION['rola']);
-    unset($_SESSION['email']);
-    header("location:prologue.php");
-}
+//require_once "security/over_uzivatela.php";
 ?>
 
 <html>
@@ -27,192 +21,203 @@
 </head>
 <body>
 
-<<<<<<< HEAD
-    <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-        <span class="navbar-brand" >Navbar</span>
-=======
-
-    <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-        <span class="navbar-brand" >
-            <?php
-                require ('config.php');
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                $conn->set_charset("UTF8");
-                $email= $_SESSION['email'];
-                if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
-                $query="SELECT meno, priezvisko from pouzivatelia WHERE email='$email'";
-                $result = mysqli_query($conn,$query);
-                while ($data = mysqli_fetch_array($result)){
-                    echo "Vitajte ".$data['meno']." ".$data['priezvisko'];
-                }
-            ?>
-        </span>
->>>>>>> 2f5b5616057e54da81cbb26ea99216ca8074d2a1
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-<<<<<<< HEAD
-                    <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-=======
-                    <a class="nav-link" href="cesty.php">Domov</a>
->>>>>>> 2f5b5616057e54da81cbb26ea99216ca8074d2a1
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Upozornenia
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item " href="#">Zapnúť</a>
-                        <a class="dropdown-item active" href="#">Vypnúť</a>
-                    </div>
-                </li>
-<<<<<<< HEAD
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log out</a></li>
-=======
-                <li class="nav-item">
-                    <a class="nav-link" href="aktuality.php">
-                        Aktuality
-                    </a>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="aktuality.php?odhlasenie='1'"><img src="logOut.png" width="25px" height="20px"></a></li>
->>>>>>> 2f5b5616057e54da81cbb26ea99216ca8074d2a1
-            </ul>
-        </div>
-    </nav>
+<!--<?php echo $_SESSION["rola"]; ?>-->
 
 
-    <div class="row">
-        <div class="col-8 leftCol">
-            <div id="myMap" style="height: 400px; width: 70%"></div>
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+    <span class="navbar-brand" >Navbar</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-            <div class="dropdown show " id="trasyId">
-                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Výber trasy
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Upozornenia
                 </a>
-
-                <div class="dropdown-menu " aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="#">Trasa 1</a>
-                    <a class="dropdown-item" href="#">Trasa 2</a>
-                    <a class="dropdown-item" href="#">Trasa 3</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item " href="#">Zapnúť</a>
+                    <a class="dropdown-item active" href="#">Vypnúť</a>
                 </div>
-            </div>
-
-            <table class="table table-dark">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-
-                </tbody>
-            </table>
-
-        </div>
-        <div class="col-4 rightCol">
-
-            <img src="img/add.png" class="addBtn" alt="add">
-
-            <div class="addDiv">
-
-                <form id="addForm">
-                    <div class="form-group">
-                        <label for="formGroupExampleInput">Zadaj štart trasy</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Zadaj štart trasy">
-                    </div>
-                    <div class="form-group">
-                        <label for="formGroupExampleInput2">Zadaj cieľ trasy</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Zadaj cieľ trasy">
-                    </div>
-
-                    <button type="button" class="btn btn-secondary">Vyhľadaj</button>
-                </form>
-
-
-                <div id="myMap2" style="height: 40%; width: 100%"></div>
-
-            </div>
-
-
-        </div>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log out</a></li>
+        </ul>
     </div>
+</nav>
 
-    <script>
 
-        $(".addBtn").on('click', function () {
-            $(".addDiv").slideToggle();
+<div class="row">
+    <div class="col-8 leftCol">
+        <div id="myMap" style="height: 400px; width: 70%"></div>
+
+        <div class="dropdown show " id="trasyId">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Výber trasy
+            </a>
+
+            <div class="dropdown-menu " aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Trasa 1</a>
+                <a class="dropdown-item" href="#">Trasa 2</a>
+                <a class="dropdown-item" href="#">Trasa 3</a>
+            </div>
+        </div>
+
+        <table class="table table-dark">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+
+            </tbody>
+        </table>
+
+    </div>
+    <div class="col-4 rightCol">
+
+        <img src="img/add.png" class="addBtn" alt="add">
+
+        <div class="addDiv">
+
+            <form id="addForm">
+                <div class="form-group">
+                    <label for="formGroupExampleInput">Zadaj štart trasy</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Zadaj štart trasy" required>
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Zadaj cieľ trasy</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Zadaj cieľ trasy" required>
+                </div>
+
+                <button type="button" id="searchBtn" class="btn btn-secondary">Vyhľadaj</button>
+                <button type="button" id="insertBtn" class="btn btn-danger">Potvrď</button>
+
+            </form>
+
+
+            <div id="myMap2" style="height: 40%; width: 100%"></div>
+
+        </div>
+
+
+    </div>
+</div>
+
+<script>
+    var place,place2;
+    var lat1,lat2,lng1,lng2;
+
+    $(".addBtn").on('click', function () {
+        $(".addDiv").slideToggle();
+    });
+
+    $("#insertBtn").on('click', function () {
+        window.location.href = "pridajTrasu.php?lat1="+lat1+"&lng1="+lng1+"&lat2="+lat2+"&lng2="+lng2+"&start="+$("#formGroupExampleInput").val()+"&end="+$("#formGroupExampleInput2").val();
+
+
+    });
+
+    var map;
+    var map2;
+    function myMap() {
+        var myLatLng = {lat: 48.6737532, lng: 19.696058};
+        var mapProp= {
+            center:myLatLng,
+            zoom:7
+        };
+        var mapProp2= {
+            center:myLatLng,
+            zoom:6
+        };
+        map = new google.maps.Map(document.getElementById("myMap"),mapProp);
+        map2 = new google.maps.Map(document.getElementById("myMap2"),mapProp2);
+
+
+        /*----------------------------------------------------------*/
+
+        var start = document.getElementById('formGroupExampleInput');
+        var ciel = document.getElementById('formGroupExampleInput2');
+
+        var autocomplete = new google.maps.places.Autocomplete(start);
+        var autocomplete2 = new google.maps.places.Autocomplete(ciel);
+
+        var marker = new google.maps.Marker({
+            map: map2,
+            anchorPoint: new google.maps.Point(0, -29)
         });
-        var map;
-        var map2;
-        function myMap() {
-            var myLatLng = {lat: 48.6737532, lng: 19.696058};
-            var mapProp= {
-                center:myLatLng,
-                zoom:7
-            };
-            var mapProp2= {
-                center:myLatLng,
-                zoom:6
-            };
-            map = new google.maps.Map(document.getElementById("myMap"),mapProp);
-            map2 = new google.maps.Map(document.getElementById("myMap2"),mapProp2);
 
+        var marker2 = new google.maps.Marker({
+            map: map2,
+            anchorPoint: new google.maps.Point(0, -29)
+        });
 
-            /*----------------------------------------------------------*/
+        autocomplete.addListener('place_changed', function() {
+            marker.setVisible(false);
+            place = autocomplete.getPlace();
+            lat1 = place.geometry.location.lat();
+            lng1 = place.geometry.location.lng();
 
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+        });
 
-            var start = document.getElementById('formGroupExampleInput');
-            var ciel = document.getElementById('formGroupExampleInput2');
+        autocomplete2.addListener('place_changed', function() {
+            marker2.setVisible(false);
+            place2 = autocomplete2.getPlace();
+            lat2 = place2.geometry.location.lat();
+            lng2 = place2.geometry.location.lng();
+            marker2.setPosition(place2.geometry.location);
+            marker2.setVisible(true);
+        });
 
-            var autocomplete = new google.maps.places.Autocomplete(start);
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
 
-            var marker = new google.maps.Marker({
-                map: map2,
-                anchorPoint: new google.maps.Point(0, -29)
-            });
+        directionsDisplay.setMap(map2);
 
-            autocomplete.addListener('place_changed', function() {
-                marker.setVisible(false);
-                var place = autocomplete.getPlace();
+        $("#searchBtn").on('click', function () {
 
-                if (place.geometry.viewport) {
-                    map2.fitBounds(place.geometry.viewport);
+            directionsService.route({
+                origin: place.geometry.location,
+                destination: place2.geometry.location,
+                travelMode: 'DRIVING'
+            }, function(response, status) {
+                if (status === 'OK') {
+                    directionsDisplay.setDirections(response);
                 } else {
-                    map2.setCenter(place.geometry.location);
-                    map2.setZoom(8);
+                    window.alert('Directions request failed due to ' + status);
                 }
-                marker.setPosition(place.geometry.location);
-                marker.setVisible(true);
             });
 
+        });
+
+        /*function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+        }*/
+
+    }
 
 
+</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArw-eyIcflcUehHyPzWx5FRzAr6EEI_68&libraries=places&callback=myMap"></script>
 
-        }
-    </script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyArw-eyIcflcUehHyPzWx5FRzAr6EEI_68&libraries=places&callback=myMap"></script>
 
-<<<<<<< HEAD
-=======
->>>>>>> Cesty -> nav, mapa, autocomplet
->>>>>>> 2f5b5616057e54da81cbb26ea99216ca8074d2a1
 </body>
 </html>
