fb62951d984d561459e67155fc603dd3432eb3ff
xradvan <xradvan@stuba.sk>
2018-05-17 17:29:03 +0200

    encodinf

--- registraciaPouzivatela.php
+++ registraciaPouzivatela.php
@@ -1,6 +1,6 @@
 <?php
-    include("security/over_uzivatela.php");
-    if(isset($_GET['odhlasenie'])){
+include("security/over_uzivatela.php");
+if(isset($_GET['odhlasenie'])){
     session_destroy();
     unset($_SESSION['rola']);
     unset($_SESSION['email']);
@@ -8,56 +8,52 @@
 }
 ?>
 
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="css/registraciaPouzivatela.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css"> 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">  
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-    <title>Používatelia</title>
-</head>
-<body>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/hide.css">
 
-<!--    Alerts -->
-<div class="alert alert-success alert-dismissible fade show d-none" role="alert" id="alert-success">
-    Užívatelia boli úspešne pridaný.
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-<div class="alert alert-danger alert-dismissible fade show d-none" role="alert" id="alert-fail">
-    Súbor sa nepodarilo nahrať. Skontrolujte veľkosť a formát. Podporovane je len kodovanie UTF-8.
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+        <link rel="stylesheet" type="text/css" href="css/registraciaPouzivatela.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
-<script>
-    // Zobrazenie alertu
-    // Uspesna registracia
-    if(window.location.href.indexOf("ok") > -1) {
-        $('#alert-success').removeClass("d-none");
-    }
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
 
-    // Nespravne meno alebo heslo
-    if(window.location.href.indexOf("fail") > -1) {
-        $('#alert-fail').removeClass("d-none");
-    }
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
 
-</script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <title>Pouzivatelia</title>
+    </head>
+    <body>
 
+    <!--    Alerts -->
+    <div class="alert alert-success  hide-default" id="alert-success">
+        Užívatelia boli úspešne pridaný.
+    </div>
+    <div class="alert alert-danger  hide-default" id="alert-fail">
+        Súbor sa nepodarilo nahrať. Skontrolujte veľkosť a formát. Podporovane je len kodovanie UTF-8.
+    </div>
+    <script>
+        // Zobrazenie alertu
+        // Uspesna registracia
+        if(window.location.href.indexOf("ok") > -1) {
+            $('#alert-success').removeClass("hide-default");
+        }
+
+        // Nespravne meno alebo heslo
+        if(window.location.href.indexOf("fail") > -1) {
+            $('#alert-fail').removeClass("hide-default");
+        }
+    </script>
     <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
         <span class="navbar-brand" >
                 <?php
@@ -71,8 +67,8 @@
                 while ($data = mysqli_fetch_array($result)){
                     echo "Vitajte ".$data['meno']." ".$data['priezvisko'];
                 }
-               
-            ?>
+
+                ?>
         </span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -103,6 +99,11 @@
                         Používatelia
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="osobneVykony.php">
+                        Osobné výkony
+                    </a>
+                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="aktuality.php?odhlasenie='1'"><img src="logOut.png" width="25px" height="20px"></a></li>
@@ -110,119 +111,116 @@
         </div>
     </nav>
     <script type="text/javascript">
-                var user = '<?php echo $_SESSION['rola']; ?>';
-                if(user=="admin"){
-                    $("#registracia").css("display","inline");
-                }
+        var user = '<?php echo $_SESSION['rola']; ?>';
+        if(user=="admin"){
+            $("#registracia").css("display","inline");
+        }
     </script>
 
-<div class="row">
-<<<<<<< HEAD
-    <div class="col-8 leftCol mt-1">
-        <br><br>
-        <h3>Tabuľka všetkých používateľov</h3>
-        <br>
-        <table class="table table-dark">
-=======
-    <div class="col-8 leftCol">
-        <h3> Tabuľka všetkých používateľov</h3>
-        <table class="table table-dark table-bordered"  id="pouzivateliaTable">
->>>>>>> 230fef517b8cc965ecf2df6d9bddcf3815f7ff41
-            <thead>
-            <tr>
-                <th scope="col"></th>
-                <th scope="col">Meno</th>
-                <th scope="col">Priezvisko</th>
-                <th scope="col">Admin</th>
-            </tr>
-            </thead>
-            <tbody>
+    <div class="row">
+        <div class="col-8 leftCol">
+            <h3> Tabuľka všetkých používateľov</h3>
+            <table class="table table-dark table-bordered"  id="pouzivateliaTable">
+                <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Meno</th>
+                    <th scope="col">Priezvisko</th>
+                    <th scope="col">Admin</th>
+                </tr>
+                </thead>
+                <tbody>
                 <?php
-                    if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
-                        $query="SELECT id, meno, priezvisko, rola from pouzivatelia";
-                        $result = mysqli_query($conn,$query);
-                        $i=1;
-                        while ($data = mysqli_fetch_array($result)){
-                            echo '<tr>';
-                            echo '<th scope="row">'.$i.'</th>';
-                            echo '<td>'.$data["meno"].'</td>';
-                            echo '<td>'.$data["priezvisko"].'</td>';
-                            if($data['rola'] == "user"){
-                                echo '<td><input type="checkbox" class="checkbox" value="'.$data['id'].'"></td>';
-                            }else{
-                                echo '<td><input type="checkbox" class="checkbox" checked value="'.$data['id'].'"></td>';
-                            }
-                             echo '</tr>';
-                             $i++;
-                        }
-                        $conn->close();
+                if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
+                $query="SELECT id, meno, priezvisko, rola from pouzivatelia";
+                $result = mysqli_query($conn,$query);
+                $i=1;
+                while ($data = mysqli_fetch_array($result)){
+                    echo '<tr>';
+                    echo '<th scope="row">'.$i.'</th>';
+                    echo '<td>'.$data["meno"].'</td>';
+                    echo '<td>'.$data["priezvisko"].'</td>';
+                    if($data['rola'] == "user"){
+                        echo '<td><input type="checkbox" class="checkbox" value="'.$data['id'].'"></td>';
+                    }else{
+                        echo '<td><input type="checkbox" class="checkbox" checked value="'.$data['id'].'"></td>';
+                    }
+                    echo '</tr>';
+                    $i++;
+                }
+                $conn->close();
 
                 ?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
 
-    </div>
-    <div class="col-4 rightCol">
-        <h3>Pridanie používateľov</h3>
-        <img src="img/add.png" class="addBtn" alt="add">
+        </div>
+        <div class="col-4 rightCol">
+            <h3>Pridanie používateľov</h3>
+            <img src="img/add.png" class="addBtn" alt="add">
 
-        <div class="addDiv">
+            <div class="addDiv">
 
-            <form id="addForm" method="POST" action="ulozenieCSV.php" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="subor">Vyberte csv súbor pre registrovanie nových používateľov </label>
-<<<<<<< HEAD
-                    <input type="file" class="form-control" name="inputFile" id="subor" required>
-=======
-                    <input type="file" id="subor" required>
->>>>>>> 230fef517b8cc965ecf2df6d9bddcf3815f7ff41
-                </div>
-                <button type="submit" id="insertBtn" class="btn btn-danger">Registrovať</button>
-            </form>
-            <table class="table table-striped" id="pridaniPouzivateliaTable" cellspacing="0" width="100%">
-            <thead>
-            <tr>
-                <th scope="col"></th>
-                <th scope="col">Meno</th>
-                <th scope="col">Priezvisko</th>
-                <th scope="col">Admin</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            </tbody>
-        </table>
+                <form id="addForm" method="post" action="ulozenieCSV.php" enctype="multipart/form-data">
+
+                    <div class="form-group">
+                        <label for="subor">Vyberte csv súbor pre registrovanie nových používateľov </label>
+                        <input type="file" id="subor" name="inputFile" required>
+                    </div>
+
+                    <button type="submit" id="insertBtn" class="btn btn-danger">Registrovať</button>
+
+                </form>
+
+                <table class="table table-striped" id="pridaniPouzivateliaTable" cellspacing="0" width="100%">
+                    <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Meno</th>
+                        <th scope="col">Priezvisko</th>
+                        <th scope="col">Admin</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
-<script>
+    <script>
 
-     $( document ).ready(function() {
-      $('#pouzivateliaTable').DataTable();
+        $( document ).ready(function() {
+            $('#pouzivateliaTable').DataTable();
 
-    $('.checkbox').change(function() {
-        if($(this).is(":checked")) {
-            var rola = "admin";
-        }else{
-            var rola = "user";
-        }
-        var id = $(this).val();
-        window.location.href = "zmenaRolyPouzivatela.php?id="+id+"&rola="+rola;
-
-              
-    });
-
-    $(".addBtn").on('click', function () {
-        $(".addDiv").slideToggle();
-    });
+            $('.checkbox').change(function() {
+                if($(this).is(":checked")) {
+                    var rola = "admin";
+                }else{
+                    var rola = "user";
+                }
+                var id = $(this).val();
+                window.location.href = "zmenaRolyPouzivatela.php?id="+id+"&rola="+rola;
 
 
+            });
 
-</script>
-</body>
-</html>
+            $(".addBtn").on('click', function () {
+                $(".addDiv").slideToggle();
+            });
+
+//            $("#insertBtn").on('click', function () {
+//                window.location.href = "ulozenieCSV.php?subor="+$("#subor").val();
+//
+//            });
+
+        });
+
+    </script>
+    </body>
+    </html>
--- ulozenieCSV.php
+++ ulozenieCSV.php
@@ -5,13 +5,6 @@
     $target_dir = "tmp/";
     $target_file = $target_dir . basename($_FILES["inputFile"]["name"]);
 
-    // Kontrola velkosti
-    if ($_FILES["inputFile"]["name"] > 500000) {
-        header("location:registraciaPouzivatela.php?upload=fail");
-        die();
-    }
-
-
     // Nahravanie
     if (move_uploaded_file($_FILES["inputFile"]["tmp_name"], $target_file)) {
 
@@ -22,7 +15,7 @@
 
         // DB pripojenie
         $conn = new mysqli($servername, $username, $password, $dbname);
-        $conn->set_charset("UTF-8");
+        $conn->set_charset("utf8");
         if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
 
         $i=0;
@@ -30,22 +23,22 @@
             if($i>0){
 
                 // Generovanie hesla
-                $heslo = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%?_"), 0, 20);
+                $heslo = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%?_"), 0, 8);
                 $hesloHash = password_hash($heslo, PASSWORD_BCRYPT);
-                $meno = iconv(mb_detect_encoding($data[2], mb_detect_order(), true), "UTF-8", $data[2]);
-                $priezvisko = iconv(mb_detect_encoding($data[1], mb_detect_order(), true), "UTF-8", $data[1]);
-                $email = iconv(mb_detect_encoding($data[3], mb_detect_order(), true), "UTF-8", $data[3]);
-                $stredna_skola = iconv(mb_detect_encoding($data[4], mb_detect_order(), true), "UTF-8", $data[4]);
-                $stredna_skola_adresa = iconv(mb_detect_encoding($data[5], mb_detect_order(), true), "UTF-8", $data[5]);
-                $ulica = iconv(mb_detect_encoding($data[6], mb_detect_order(), true), "UTF-8", $data[6]);
-                $psc = iconv(mb_detect_encoding($data[7], mb_detect_order(), true), "UTF-8", $data[7]);
-                $obec = iconv(mb_detect_encoding($data[8], mb_detect_order(), true), "UTF-8", $data[8]);
-                $import="INSERT into pouzivatelia(meno,priezvisko,email,heslo,stredna_skola,stredna_skola_adresa,ulica,psc,obec)values('".$meno."','".$priezvisko."','".$email."','".$hesloHash."','".$stredna_skola."','".$stredna_skola_adresa."','".$ulica."','".$psc."','".$obec."')";
 
-                echo mb_detect_encoding($priezvisko, mb_detect_order(), true)."<br>";
-                echo $import;
+                $meno = $data[2];
+                $priezvisko = $data[1];
+                $email = $data[3];
+                $stredna_skola = $data[4];
+                $stredna_skola_adresa = $data[5];
+                $ulica = $data[6];
+                $psc = $data[7];
+                $obec = $data[8];
+
+                $import="INSERT into pouzivatelia(meno,priezvisko,email,heslo,stredna_skola,stredna_skola_adresa,ulica,psc,obec) values('".$meno."','".$priezvisko."','".$email."','".$hesloHash."','".$stredna_skola."','".$stredna_skola_adresa."','".$ulica."','".$psc."','".$obec."')";
+
                 // Query
-//                mysqli_query($conn,$import) or die(mysqli_error($conn));
+                mysqli_query($conn,$import) or die(mysqli_error($conn));
             }
             $i=1;
         }
@@ -60,8 +53,8 @@
         // die();
 
     } else {
-        header("location:registraciaPouzivatela.php?upload=fail");
-        die();
+//        header("location:registraciaPouzivatela.php?upload=fail");
+//        die();
     }
 
 ?>
