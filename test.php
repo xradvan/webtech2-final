28b1d8f17e118b81e070ca39a4e2fd79e91ccc84
MarcelBoldis <marcelboldis@gmail.com>
2018-05-18 20:08:33 +0200

    ach

--- test.php
+++ test.php
@@ -7,10 +7,23 @@ if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
 $values = $_SERVER['REQUEST_URI'];
 parse_str($_SERVER['QUERY_STRING'], $output);
 
-for ($i = 1; $i < count($output); $i++){
-    $query="UPDATE pouzivatelia SET nazovtimu = '".$output['tim']."' WHERE id = ".$output[$i];
+$query2="INSERT INTO tim  (nazov) VALUES ('".$output['tim']."')";
+$result2 = mysqli_query($conn,$query2);
 
+for ($i = 1; $i < count($output); $i++){
+
+    $query = "SELECT id from tim ORDER BY id DESC LIMIT 1";
     $result = mysqli_query($conn,$query);
+    $idT = 0;
+
+    while ($data = mysqli_fetch_array($result)){
+
+        $idT = $data['id'];
+
+    }
+    $query3="UPDATE pouzivatelia SET id_timu = ".$idT." WHERE id = ".$output[$i];
+    $result3 = mysqli_query($conn,$query3);
+
 }
 echo "<script>window.location.href ='stafetovyMod.php'</script>";
 ?>
