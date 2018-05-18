ec9d12b64932cd95d8f05022251f9b9495df65b1
Nikola Carnogurska <carnogurska.n@gmail.com>
2018-05-19 00:04:01 +0200

    dokumentacia aplikacie hotova

--- dokumentacia.php
+++ dokumentacia.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="./css/prologue.css">
+<link rel="stylesheet" href="css/dokumentacia.css">
     <title>Dokumentácia</title>
 </head>
 <body>
@@ -29,7 +29,8 @@
     </ul>
 </div>
 </nav>
-    <div class="col-lg-6">
+<div class="container">
+    <div class="col-lg-6" >
         <table class="table table-dark table-bordered text-center"  id="pouzivateliaTable">
         <h1 class="text-center">Rozdelenie úloh</h1>
             <thead>
@@ -45,128 +46,136 @@
             <tbody>
             <tr>
             <td scope="col">Verzionovací systém Github</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
             </tr>
             <tr>
             <td scope="col">Registrácia</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
             </tr>
             <tr>
             <td scope="col">Registrácia adminom</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
             </tr>
             <tr>
             <td scope="col">Prihlásenie</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
             </tr>
             <tr>
             <td scope="col">Definovanie trasy</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">Nastavenie módu trasy</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
             </tr>
             <tr>
             <td scope="col">Tabuľka s trasami (aktivácia/deaktivácia)</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
             </tr>
             <tr>
             <td scope="col">Štafetový mód</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">Grafické odlíšenie členov trasy</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">Tabuľka osobných výkonov</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">Tabuľka všetkých používateľov pre admina</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">Generovanie PDF súboru</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">Úvodná strana</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">Aktualizácia údajov v tabuľke výkonov</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             <tr>
             <td scope="col">pridávanie aktualít s odoslaním na e-mail</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-success">A</td>
-            <td scope="col" class="bg-danger">N</td>
-            <td scope="col" class="bg-danger">N</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
+            </tr>
+            <tr>
+            <td scope="col">Dokumentácia</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="yes">A</td>
+            <td scope="col" class="no">N</td>
+            <td scope="col" class="no">N</td>
             </tr>
             </tbody>
             </table>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-6" style="padding-left: 6em;">
             <h1 class="text-center">Technická dokumentácia</h1>
             <h3>Použité knižnice / frameworky</h3>
             <ul>
@@ -184,6 +193,40 @@
             <ul>
                 <li><a href="https://github.com/xradvan/webtech2-final">Github</a></li>
             </ul>
+            </div>
+        </div>
+            <div class="container">
+                <h1>Používateľská príručka</h1>
+                <p>Táto webová aplikácia je súčasťou semestrálneho zadania predmetu Webové technológie 2 študentov FEI STU API. Jej zaznamenávať jednotlivé trasy tréningu používateľov, zobrazovanie výkonov, odoberanie aktualít a definovanie nových trás, ktoré používateľ odbehne.
+                </p>
+                <p>Registrácia je prístupná pre každého, kto chce mať prehľad o svojich tréningoch. Aplikácia umožňuje administrátorovi spravovať trasy, určovať práva používateľov, vytvárať tímy pre štafetový mód a pridávať aktuality. Používateľ si môže vybrať z viacerých typov módov trás a to:</p>
+            <ul>
+                <li>Štafetový - Na jednej trase môžu byť priradené viaceré tímy, výkony a trasy vidia všetci používatelia webovej aplikácie</li>
+                <li>Verejný - Výkony a trasy vidia všetci používatelia webovej aplikácie</li>
+                <li>Privátny - Výkony a trasy vidí len práve prihlásený používateľ a administrátor webovej aplikácie</li>
+            </ul>
+            <h4><strong>Úvodná stránka</strong></h4>
+            <p>Na úvodnej stránke si môže neprihlásený používateľ zobraziť adresy alebo školy všetkých zaregistrovaných používateľov prostredníctvom mapy. Stránka slúži aj na prihlásenie alebo registráciu, na ktorú sa dá dostať z pravého rohu navigačného panela.</p>
+            <img src="img/prologue.png" width="100%">
+            <h4><strong>Domovská obrazovka</strong></h4>
+            <ul>
+                <li>Administrátor - Má k dispozícii definovanie trasy, pridanie tímu pre štafetový mód, zobrazenie vybranej trasy ako aj zmenu aktívnej trasy. Pri výbere trasy sa zobrazí graf výkonov jednotlivých používateľov, respektíve tímov. Administrátor vidí trasy vo verejnom, privátnom a štafetovom móde</li>
+                <li>Bežný používateľ - Má k dispozícii vytvorenie trasy v privátnom móde, prezeranie svojích trás v privátnom móde a v štafetovom a taktiež vidí všetky trasy vo verejnom móde. Používateľ si môže pridať prostredníctvom formuláru tréning na vybranej trase. Svoje trasy môže zaktivovať alebo zdeaktivovať, pričom je vždy len jedna aktívna</li>
+            </ul>
+            <img src="img/cesty.png" width="100%">
+            <h4><strong>Aktuality</strong></h4>
+             <ul>
+                <li>Administrátor - vidí všetky aktuality, pričom môže pridať novú aktualitu, či ju zmeniť alebo zmazať. Pri pridaní trasy sa odošle prostredníctvom e-mailu upozornenie všetkým používateľom, ktorí sú odberateľmi aktualít.</li>
+                <li>Bežný používateľ - Vidí všetky aktuality, ktoré pridal administrátor</li>
+            </ul>
+            <h4><strong>Osobné výkony</strong></h4>
+            <p>Používateľ má k dispozícii prehľadnú tabuľku všetkých svojích výkonov a taktiež si ju môže stiahnuť vo forme PDF.</li>
+            </p>
+            <h4><strong>Používatelia</strong></h4>
+            <p>
+                Stránka je viditeľná len pre prihláseného administrátora. Poskytuje zoznam všetkých registrovaných používateľom, pričom im môže nastaviť práva a zobraziť ich osobné výkony. Na tejto stránke vie taktiež hromadne zaregistrovať nových používateľov prostredníctvom CSV súboru.
+            </p>
+            <img src="img/pouzivatelia.png" width="100%">
             </div>
             
 </body>
